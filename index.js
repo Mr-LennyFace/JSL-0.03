d7a1ffce4bac5da211dbe300964fc79d7f108ef5
Mr-LennyFace <explorist20@gmail.com>
2021-05-20 14:00:49 +0530

    1 file modified

--- index.js
+++ index.js
@@ -33,3 +33,13 @@ function number(num) {
   return num * 5;
 }
 console.log(number(12));
+
+var changed = 0;
+
+function change(num) {
+  return (num + 5) / 3;
+}
+
+changed = change(10);
+
+console.log(changed);
