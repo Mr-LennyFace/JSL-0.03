0270b631458338eb14698eefdc818e9078547e9a
Mr-LennyFace <explorist20@gmail.com>
2021-05-20 13:19:52 +0530

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Open the Console</h1>`;
+
+// Shopping List
+
+var shoppinglist = [
+  ['Milk', 2],
+  ['Cornflaxes', 1],
+  ['Meat (1kg)', 2],
+  ['Tomatoes (1kg)', 2],
+  ['Watermelons', 2],
+  ['Eggs', 25],
+  [('Chicken 65 masala packet (100g)', 10)],
+  ['Rice (1kg)', 1]
+];
